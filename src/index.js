e9e693fd1c9c9356a8c91a7fbf4ea463cb1f3d9b
Lucas <lucasfelicio378@email.com>
2022-09-15 01:33:18 -0300

    post and get books

--- src/index.js
+++ src/index.js
@@ -1,31 +1,12 @@
 /* eslint-disable require-jsdoc */
 import express from 'express';
 import cors from 'cors';
-import Joi from 'joi';
 import {postCadastro, getCadastro} from './cadastro.js';
 import {postLogin, Delete} from './login.js';
+import {postBook, getBook} from './books.js';
+import {validaCadastro, validaBook} from './validations.js';
 
 const app = start();
-
-async function validaCadastro(req, res, next) {
-  const validation = schemaRegistration.validate(req.body, {
-    abortEarly: true,
-  });
-
-  if (validation.error) {
-    return res.sendStatus(422);
-  }
-
-  next();
-}
-
-const schemaRegistration = Joi.object().keys({
-  name: Joi.string().min(1).required(),
-  email: Joi.string()
-      .email({minDomainSegments: 2, tlds: {allow: ['com', 'net']}})
-      .required(),
-  password: Joi.string().min(6).required(),
-});
 
 app.post('/cadastro', validaCadastro, postCadastro);
 
@@ -34,6 +15,10 @@ app.get('/cadastro', getCadastro);
 app.post('/login', postLogin);
 
 app.delete('/logout/:token', Delete);
+
+app.post('/sold', validaBook, postBook);
+
+app.get('/home/:genre', getBook);
 
 app.listen(5000);
 
---
+++ src/validations.js
@@ -0,0 +1,45 @@
+/* eslint-disable require-jsdoc */
+import Joi from 'joi';
+
+export async function validaCadastro(req, res, next) {
+  const validation = schemaRegistration.validate(req.body, {
+    abortEarly: true,
+  });
+
+  if (validation.error) {
+    return res.sendStatus(422);
+  }
+
+  next();
+}
+
+export async function validaBook(req, res, next) {
+  const validation = schemaBook.validate(req.body, {
+    abortEarly: true,
+  });
+
+  if (validation.error) {
+    return res.send('deu ruim');
+  }
+
+  next();
+}
+
+const schemaRegistration = Joi.object().keys({
+  name: Joi.string().min(1).required(),
+  email: Joi.string()
+      .email({minDomainSegments: 2, tlds: {allow: ['com', 'net']}})
+      .required(),
+  password: Joi.string().min(6).required(),
+});
+
+const schemaBook = Joi.object().keys({
+  name: Joi.string().min(1).required(),
+  pag_number: Joi.number().min(1).required(),
+  description: Joi.string().min(1).required(),
+  autor: Joi.string().min(1).required(),
+  url: Joi.string().min(1).required(),
+  number_solds: Joi.number().required(),
+  price: Joi.number().required(),
+  genre: Joi.string().required(),
+});
