984c4ae231d1ffa84060b456fee5a6d57c03cca4
Lucas <lucasfelicio378@email.com>
2022-09-19 16:29:20 -0300

    test

--- src/routes/loginRoutes.js
+++ src/routes/loginRoutes.js
@@ -1,12 +1,10 @@
 /* eslint-disable new-cap */
 import express from 'express';
-import {postLogin, Delete} from '../src/controller/login.js';
+import {postLogin} from '../src/controller/login.js';
 
 const router = express.Router();
 
 router.post('/login', postLogin);
-
-router.delete('/logout', Delete);
 
 export default router;
 
